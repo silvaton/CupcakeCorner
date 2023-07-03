90710cb3dd11284e5389941e5b962faeb6e8fe77
silvaton <tdasilva@ext.laligatech.com>
2023-07-03 17:54:12 +0200

    initial Setup

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Ton Silva on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
